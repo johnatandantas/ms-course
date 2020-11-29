7ae714d53b40cbe9fc1a23ae8f8fe7b61f264819
Johnatan Dantas <dantas.johnatan@gmail.com>
2020-11-29 14:15:29 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/johnworks/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.johnworks.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.johnworks.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
